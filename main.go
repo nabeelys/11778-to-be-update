ae8ba84973afa1cd06e9da0628db4f115662e499
NabeelSaabna <nabeel.saabna@whitesourcesoftware.com>
2022-11-17 14:43:51 +0200

    edit files

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 import _ "github.com/nabeelys/i11778-monorepo"
 import _ "github.com/nabeelys/i11778-monorepo/multiply"
+import _ "sigs.k8s.io/kustomize/api/filesys"
+import _ "sigs.k8s.io/kustomize/kyaml"
 
 func main() {
 	fmt.Println()
